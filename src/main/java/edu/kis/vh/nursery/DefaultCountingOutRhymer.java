4d8567946611c445162779eb956a30961215e734
DogyXX <kacperjedrzejczyk7312@gmail.com>
2026-03-19 13:10:26 +0100

    7. Dodanie modyfikatora final

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -5,7 +5,7 @@ public class DefaultCountingOutRhymer {
     private static final int EMPTY = -1;
     private static final int CAPACITY = 12;
 
-    private int[] numbers = new int[CAPACITY];
+    private final int[] numbers = new int[CAPACITY];
 
     public int total = EMPTY;
 
